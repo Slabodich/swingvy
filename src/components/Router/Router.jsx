112b61cca4364895d97c39d564f05823f39fad1a
John Doe <johndoe@example.com>
2023-06-05 15:57:06 +0300

    add gh-page

--- src/components/Router/Router.jsx
+++ src/components/Router/Router.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import List from "../Pages/List/List";
-import NewsDetail from "../Pages/NewsDetail/NewsDetail";
-import Profile from "../Pages/Profile/Profile";
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
-import styles from "./Router.module.css";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import List from '../Pages/List/List';
+import NewsDetail from '../Pages/NewsDetail/NewsDetail';
+import Profile from '../Pages/Profile/Profile';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import styles from './Router.module.css';
 
 const Router = () => {
   return (
@@ -14,7 +14,7 @@ const Router = () => {
         <Header />
         <div className={styles.main}>
           <Routes>
-            <Route element={<List />} path="/" />
+            <Route element={<List />} path="swingvy/" />
             <Route element={<NewsDetail />} path="/news/:id" />
             <Route element={<Profile />} path="/profile" />
             <Route path="*" element={<div>Страница не найдена</div>} />
